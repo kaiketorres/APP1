4988a0a9d1146da49f5c388de1acdf0c397e85b4
kaiketorres <kaiketorrea@gmail.com>
2024-07-01 18:45:51 -0300

    upando modificacoes

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App1() {
+//   return (
+//     <View style={styles.container}>
+//       <View>
+//         <Text>CFB cursos</Text>
+//         <Text>Curso de react native</Text>
+//       </View>
+//       <View>
+//         <Text>Aula 3</Text>
+//         <Text>www.cfbcursos.com.br</Text>
+//       </View>
+//     </View>
+//   )
+// }
+
+export default class App1 extends Component{
+  render(){
+    return(
+      <View>
+        <Text>CFB cursos</Text>
+         <Text>Curso de react native</Text>
+       </View>
+    )
+  }
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
